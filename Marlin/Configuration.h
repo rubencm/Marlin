8a9b84d1862d9cc168340829fa15fce570b56e8a
Rubén Calvo <rubencm@gmail.com>
2017-11-12 15:26:15 +0100

    Update extruder steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -531,7 +531,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 103 }
 
 /**
  * Default Max Feed Rate (mm/s)
